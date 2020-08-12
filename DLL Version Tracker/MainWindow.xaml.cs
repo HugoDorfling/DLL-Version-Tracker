f019c86204a91b1c9aab94cff86029a743cb6408
Hugo Dorfling <hugodorfling@live.co.za>
2020-08-12 12:41:40 +0200

    error handling + getAverageAssemblyVersion + Make window draggable + add website link

--- DLL Version Tracker/MainWindow.xaml.cs
+++ DLL Version Tracker/MainWindow.xaml.cs
@@ -19,12 +19,9 @@ using System.Text.RegularExpressions;
 
 namespace DLL_Version_Tracker
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        private string filePath = @"C:\";
+        private string filePath = @"";
 
         public MainWindow()
         {
@@ -39,26 +36,29 @@ namespace DLL_Version_Tracker
 
         public void loadDllFiles()
         {
-            
-            filePath = filePathTextBox.Text;
-            List<string> dllFiles = Directory.GetFiles(filePath, "*.dll", SearchOption.AllDirectories).ToList();
-            List<string> exeFiles = Directory.GetFiles(filePath, "*.exe", SearchOption.AllDirectories).ToList();
-            List<string> thirdPartyPackages = new List<string>() { "System" };
+            try
+            {
+                outputListView.Items.Add("START FILE SEARCH: ");
+                outputListView.Items.Add("-------------------");
+                filePath = filePathTextBox.Text;
+                List<string> dllFiles = Directory.GetFiles(filePath, "*.dll", SearchOption.AllDirectories).ToList();
+                List<string> exeFiles = Directory.GetFiles(filePath, "*.exe", SearchOption.AllDirectories).ToList();
+                List<string> thirdPartyPackages = new List<string>() { "System" };
+                List<string> assemblyVersions = new List<string>();
+                thirdPartyPackages = getThirdPartyNames();
 
-            thirdPartyPackages = getThirdPartyNames();
-
-            foreach (string file in dllFiles)
+                foreach (string file in dllFiles)
                 {
                     var info = new FileInfo(file);
                     string fileName = info.Name;
 
                     bool isThirdParty = checkIsThirdPartyLibrary(fileName, thirdPartyPackages);
-                    
+
                     if (!isThirdParty)
                     {
                         var assemblyVersion = AssemblyName.GetAssemblyName(info.FullName).Version;
+                        assemblyVersions.Add(assemblyVersion.ToString());
                         string fileNameAndAssembly = fileName + "  -  DLL Version: " + assemblyVersion.ToString();
-
                         outputListView.Items.Add(fileNameAndAssembly);
                     }
                 }
@@ -67,11 +67,57 @@ namespace DLL_Version_Tracker
                     var info = new FileInfo(file);
                     string fileName = info.Name;
                     var assemblyVersion = AssemblyName.GetAssemblyName(info.FullName).Version;
+                    assemblyVersions.Add(assemblyVersion.ToString());
                     string fileNameAndAssembly = fileName + "  -  EXE Version: " + assemblyVersion.ToString();
 
                     outputListView.Items.Add(fileNameAndAssembly);
                 }
+                string averageVersion = getAverageAssemblyVersion(assemblyVersions);
 
+                outputListView.Items.Add("-------------------");
+                outputListView.Items.Add("END FILE SEARCH");
+                outputListView.Items.Add("");
+                outputListView.Items.Add("**AVERAGE VERSION:**");
+                outputListView.Items.Add(averageVersion);
+                outputListView.Items.Add("");
+                outputListView.Items.Add(@"!!! POTENTIALLY OUTDATED OR ERRONEOUS LIBRARIES !!!");
+                foreach (string file in dllFiles)
+                {
+                    var info = new FileInfo(file);
+                    string fileName = info.Name;
+
+                    bool isThirdParty = checkIsThirdPartyLibrary(fileName, thirdPartyPackages);
+
+                    if (!isThirdParty)
+                    {
+                        var assemblyVersion = AssemblyName.GetAssemblyName(info.FullName).Version;
+                        assemblyVersions.Add(assemblyVersion.ToString());
+                        string fileNameAndAssembly = fileName + "  -  DLL Version: " + assemblyVersion.ToString();
+                        if (assemblyVersion.ToString() != averageVersion)
+                        {
+                            outputListView.Items.Add(fileNameAndAssembly);
+                        }
+
+                    }
+                }
+                foreach (string file in exeFiles)
+                {
+                    var info = new FileInfo(file);
+                    string fileName = info.Name;
+                    var assemblyVersion = AssemblyName.GetAssemblyName(info.FullName).Version;
+                    assemblyVersions.Add(assemblyVersion.ToString());
+                    string fileNameAndAssembly = fileName + "  -  EXE Version: " + assemblyVersion.ToString();
+                    if (assemblyVersion.ToString() != averageVersion)
+                    {
+                        outputListView.Items.Add(fileNameAndAssembly);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("ERROR! - " + e.Message.ToString());
+                outputListView.Items.Clear();
+            }
         }
 
         private List<string> getThirdPartyNames()
@@ -101,7 +147,8 @@ namespace DLL_Version_Tracker
             }
             catch (Exception e)
             {
-                MessageBox.Show("Error!" + e.Message.ToString());
+                MessageBox.Show("ERROR! - " + e.Message.ToString());
+                outputListView.Items.Clear();
             }
             return thirdPartyPackages;
         }
@@ -126,15 +173,61 @@ namespace DLL_Version_Tracker
                 if (m.Success)
                 {
                     isThirdParty = true;
-                    MessageBox.Show("Third Party Library found and excluding from list: " + filename);
                 }
             }
             return isThirdParty;
         }
 
+        private string getAverageAssemblyVersion(List<string> versions)
+        {
+            string averageVersion;
+            var freqs = GetFrequencies(versions);
+            averageVersion = DisplaySortedFrequencies(freqs);
+
+            return averageVersion;
+        }
+
+        private Dictionary<string,int> GetFrequencies(List<string> versions)
+        {
+            var result = new Dictionary<string, int>();
+            foreach (string value in versions)
+            {
+                if (result.TryGetValue(value, out int count))
+                {
+                    result[value] = count + 1;
+                }
+                else
+                {
+                    result.Add(value, 1);
+                }
+            }
+            return result;
+        }
+
+        private string DisplaySortedFrequencies(Dictionary<string, int> frequencies)
+        {
+            var sorted = from pair in frequencies
+                         orderby pair.Value descending
+                         select pair;
+
+            return sorted.ElementAt(0).Key;
+        }
+
         private void closeButton_Click(object sender, RoutedEventArgs e)
         {
             System.Windows.Application.Current.Shutdown();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                DragMove();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender,
+                                       System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.Uri.AbsoluteUri);
         }
     }
 }
